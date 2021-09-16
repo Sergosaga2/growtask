e57fe0717b0499d49067b4f480061d3ea43b5580
Sergey <sergosaga2@gmail.com>
2021-09-16 19:56:42 +0500

    Добавил слайдеры и форму

--- js/normalize.js
+++ js/normalize.js
@@ -7,4 +7,41 @@ $( document ).ready(function() {
             $('.header').removeClass('headerScroll');
         }
     });
+
+    $(".owl-carousel-1").owlCarousel({
+        items: 1,
+        touchDrag: true,
+        nav: true,
+        dots: false,
+        loop: true
+    });
+    $(".owl-carousel-2").owlCarousel({
+        items: 3,
+        margin: 40,
+        touchDrag: true,
+        nav: false,
+        dots: false,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            }
+        }
+    });
+    
+    var owl=$(".owl-carousel-2");
+	owl.owlCarousel();
+	$(".next").click(function(){
+		owl.trigger("next.owl.carousel");
+	});
+	$(".prev").click(function(){
+		owl.trigger("prev.owl.carousel");
+	});
+          
 });
