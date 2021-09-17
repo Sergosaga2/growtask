120909ada6eff2ac033f5978e06103102381111b
Sergey <sergosaga2@gmail.com>
2021-09-17 19:39:52 +0500

    Почти всё сделал

--- js/normalize.js
+++ js/normalize.js
@@ -8,6 +8,30 @@ $( document ).ready(function() {
         }
     });
 
+    $(".nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
+    $('.btn-menu').on('click', () => {
+        $('.btn-menu').toggleClass('btn-menu-active');
+        $('.dropdown').toggleClass('dropdownActive');
+        $('body').toggleClass("bodyNoneScroll");
+    });
+
+    $('.openModal').on('click', (e) => {
+        $('.modalWrapper').addClass('modalWrapperActive');
+        $('.global').addClass('blur');
+        e.preventDefault();
+    });
+    $('.closedImg').on('click', (e) => {
+        $('.modalWrapper').removeClass('modalWrapperActive');
+        $('.global').removeClass('blur');
+        e.preventDefault();
+    });
+
     $(".owl-carousel-1").owlCarousel({
         items: 1,
         touchDrag: true,
@@ -42,6 +66,27 @@ $( document ).ready(function() {
 	});
 	$(".prev").click(function(){
 		owl.trigger("prev.owl.carousel");
-	});
-          
+    });
+    
+    $('.accordeonItem').on('click', function() {
+        $(this).find('.accordeionTextBlock').slideToggle(150);
+		$(this).find('.accordeionTitleBlock').toggleClass('accordeionTitleBlockActive');
+        $(this).find('.accordImg').toggleClass('accordImgActive');
+        $('.accordeonItem').not(this).find('.accordeionTextBlock').hide(150);
+        $('.accordeonItem').not(this).find('.accordeionTitleBlock').removeClass('accordeionTitleBlockActive');
+        $('.accordeonItem').not(this).find('.accordImg').removeClass('accordImgActive');
+    });
+    $(document).mouseup(function (e){
+        var accordeonItem = $('.accordeonItem');
+        var accordeonTitle = $('.accordeionTitleBlock')
+        var accordeonContent = $('.accordeionTextBlock');
+        var accordeonArrow = $('.accordImg');
+        if (!accordeonItem.is(e.target) && accordeonItem.has(e.target).length === 0) {
+          accordeonContent.hide(150);
+          accordeonArrow.removeClass('accordImgActive');
+          accordeonTitle.removeClass('accordeionTitleBlockActive');
+        };
+        e.stopPropagation();
+      });
+    
 });
